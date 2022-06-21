d583fc6ea352c240eb0fe04b28cc45dfe5f28316
ALWaleedRizq <waleedrizq07@gmail.com>
2022-06-22 01:20:16 +0800

    First commit for new repository

---
+++ src/main/java/org/potential_java/keep_learning/App.java
@@ -0,0 +1,13 @@
+package org.potential_java.keep_learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
